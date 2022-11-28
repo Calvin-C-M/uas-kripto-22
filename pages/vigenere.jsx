1ffbc6e7ed1ad5f9fe6d239c8e10b7fd725b6961
Calvin-C-M <montolalu28@gmail.com>
2022-11-29 02:45:34 +0700

    Added vigenere page UI

--- pages/vigenere.jsx
+++ pages/vigenere.jsx
@@ -1,8 +1,61 @@
+import { useState } from "react";
+
 const Vigenere = () => {
+    const [text, setText] = useState("")
+    const [key, setKey] = useState("")
+    const [resultText, setResultText] = useState("")
+
+    const encrypt = () => {
+        
+    }
+
+    const decrypt = () => {
+
+    }
+
     return (
         <>
             <section>
-                
+                <header className="mb-5">
+                    <h1 className="text-4xl">Vigenere Cipher</h1>
+                </header>
+                <section className="flex gap-3 container bg-slate-700 rounded-lg p-5">
+                    <section id="form" className="container">
+                        <div className="mb-3">
+                            <label htmlFor="text" className="label">
+                                Enter Text
+                            </label>
+                            <textarea 
+                                name="text"
+                                id="text"
+                                className="textarea textarea-primary w-80 resize-none"
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="key" className="label">
+                                Enter Key
+                            </label>
+                            <input 
+                                type="text" 
+                                name="key" 
+                                id="key" 
+                                className="input input-primary w-full max-w-xs"
+                                onChange={e => setKey(e.target.value)}
+                            />
+                        </div>                        
+                        <div className="flex gap-3">
+                            <button className="btn btn-primary" onClick={() => encrypt()}>
+                                Encrypt
+                            </button>
+                            <button className="btn btn-primary" onClick={() => decrypt()}>
+                                Decrypt
+                            </button>
+                        </div>
+                    </section>
+                    <section className="container bg-slate-900 p-3 rounded-lg">
+                        {resultText}
+                    </section>
+                </section>
             </section>
         </>
     );
