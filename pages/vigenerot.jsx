e1525ae9afb8e10be1211672ed405ff6fcc20367
Calvin-C-M <montolalu28@gmail.com>
2022-12-07 03:06:36 +0700

    Added vigenerot13 algorithm to page

--- pages/vigenerot.jsx
+++ pages/vigenerot.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Rot13Shift from "../functions/Rot13Shift";
+import VigenereEncrypt from "../functions/VigenereEncrypt";
+import VigenereDecrypt from "../functions/VigenereDecrypt";
 
 const VigeneRot = () => {
+    const textInput = useRef(null)
+    const keyInput = useRef(null)
+
     const [text, setText] = useState("")
     const [key, setKey] = useState("")
+    const [algorithmFirst, setAlgorithmFirst] = useState("VIGENERE")
     const [resultText, setResultText] = useState("")
+
+    const handleEncrypt = () => {
+        if(algorithmFirst == "VIGENERE") {
+            setResultText(Rot13Shift(VigenereEncrypt(text, key)))
+        } else {
+            setResultText(VigenereEncrypt(Rot13Shift(text), key))
+        }
+    }
+
+    const handleDecrypt = () => {
+        if(algorithmFirst == "VIGENERE") {
+            setResultText(Rot13Shift(VigenereDecrypt(text, key)))
+        } else {
+            setResultText(VigenereDecrypt(Rot13Shift(text), key))
+        }
+    }
+
+    const clearCalc = () => {
+        textInput.current.value = ""
+        keyInput.current.value = ""
+        setResultText("")
+    }
 
     return (
         <>
@@ -18,6 +47,7 @@ const VigeneRot = () => {
                                 Enter Text
                             </label>
                             <textarea 
+                                ref={textInput}
                                 name="text"
                                 id="text"
                                 className="textarea textarea-primary w-80 resize-none"
@@ -29,19 +59,37 @@ const VigeneRot = () => {
                                 Enter Key
                             </label>
                             <input 
+                                ref={keyInput}
                                 type="text" 
                                 name="key" 
                                 id="key" 
                                 className="input input-primary w-full max-w-xs"
                                 onChange={e => setKey(e.target.value)}
                             />
-                        </div>                        
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="encrypt-first" className="label">
+                                Algorithm used first?
+                            </label>
+                            <select 
+                                name="encrypt-first" 
+                                id="encrypt-first" 
+                                className="select"
+                                onChange={e => setAlgorithmFirst(e.target.value)}
+                            >
+                                <option value="VIGENERE">Vigenere</option>
+                                <option value="ROT13">ROT13</option>
+                            </select>
+                        </div>
                         <div className="flex gap-3">
-                            <button className="btn btn-primary" onClick={() => encrypt()}>
+                            <button className="btn btn-primary" onClick={() => handleEncrypt()}>
                                 Encrypt
                             </button>
-                            <button className="btn btn-primary" onClick={() => decrypt()}>
+                            <button className="btn btn-primary" onClick={() => handleDecrypt()}>
                                 Decrypt
+                            </button>
+                            <button className="btn btn-error" onClick={() => clearCalc()}>
+                                Clear
                             </button>
                         </div>
                     </section>
